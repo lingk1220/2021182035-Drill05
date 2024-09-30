5ab61d13e9d4703dbbe710b25310ba81245049ea
lingk4231075 <strbryjam1220@gmail.com>
2024-09-30 22:13:11 +0900

    스프라이트 변경 및 위아래키 구현

--- move_character_with_key.py
+++ move_character_with_key.py
@@ -3,12 +3,13 @@ from pico2d import *
 
 open_canvas(960, 750)
 background = load_image('TUK_GROUND.png')
-character = load_image('Dash.png')
+character = load_image('walk.png')
 
 
 def handle_events():
     global running
-    global dir
+    global dir_x
+    global dir_y
 
     events = get_events()
     for event in events:
@@ -16,32 +17,41 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1
+                dir_x += 1
             elif event.key == SDLK_LEFT:
-                dir -= 1
+                dir_x -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
 
         if event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                dir -= 1
+                dir_x -= 1
             elif event.key == SDLK_LEFT:
-                dir += 1
+                dir_x += 1
+            if event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
 
 running = True
-x = 800 // 2
+x = 960 // 2
 frame = 0
-dir = 0
+dir_x = 0
+dir_y = 0
 
 while running:
     clear_canvas()
     background.draw(480, 375, 960, 750)
-    character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+    character.clip_composite_draw(frame * 128, 0, 128, 128, 0, 'h', x, 130, 200, 200)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 6
-    x += dir * 5
-    delay(0.05)
+    x += dir_x * 5
+    delay(0.1)
 
 
 close_canvas()
